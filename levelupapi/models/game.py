dba94de20be1f68e160f17d61e5b0eec9e639cdf
nathanRomad <nathan.a.hamilton@gmail.com>
2021-05-04 15:02:22 -0500

    fix: migrated models into db

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -2,7 +2,7 @@ from django.db import models
 from .gameType import GameType
 
 class Game(models.Model):
-    title : models.CharField(max_length=50)
-    difficulty : models.CharField(max_length=50)
-    numberOfPlayers : models.CharField(max_length=50)
-    game_type : models.ForeignKey(GameType, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    difficulty = models.IntegerField()
+    numberOfPlayers = models.IntegerField()
+    game_type = models.ForeignKey(GameType, on_delete=models.CASCADE)
--- levelupapi/models/player.py
+++ levelupapi/models/player.py
@@ -3,5 +3,5 @@ from .gamer import Gamer
 from .event import Event
 
 class Player(models.Model):
-    gamer : models.ForeignKey(Gamer, on_delete=models.CASCADE)
-    event : models.ForeignKey(Event, on_delete=models.CASCADE)
+    gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
